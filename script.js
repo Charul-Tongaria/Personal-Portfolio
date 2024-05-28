1c656c16824b3e7a4896a8d6ac39f07279965171
Charul-Tongaria <charultongaria.1703@poornima.org>
2024-05-28 14:50:18 +0530

    Second

--- script.js
+++ script.js
@@ -2,26 +2,3 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
-
-// contact.js
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    // Get form values
-    const name = document.getElementById("name").value;
-    const contact = document.getElementById("contact").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    // Use EmailJS or any other email service to send the form data
-    emailjs.send('service_ay9dzvy', '__ejs-test-mail-service__', {
-        name: name,
-        contact: contact,
-        email: email,
-        message: message,
-    }).then(function(response) {
-        alert('Your message has been sent successfully!');
-    }, function(error) {
-        alert('There was an error sending your message. Please try again later.');
-    });
-});
